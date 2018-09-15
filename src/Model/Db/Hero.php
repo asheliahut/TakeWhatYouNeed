47b77f826d81c314fa62c259945e6b6b70e3696f
Ashley Hutson <ashley.hutson@mheducation.com>
2018-09-15 09:22:08 -0700

    remove wrongfully auto added copywrite

--- src/Model/Db/Hero.php
+++ src/Model/Db/Hero.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Copyright (c) 2017-2018 McGraw-Hill Education <http://mheducation.com>.
- *
- * All rights reserved, unless this code has already been open-sourced elsewhere.
- */
 
 declare(strict_types=1);
 
